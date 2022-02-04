8e1d0a6dec6b673367b5c159d41c7e38ace863c9
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2022-02-04 17:54:16 -0300

    feat: aceita evento do workflow para buscar commit

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  const octokit = github.getOctokit(core.getInput("github_token"));
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const octokit = github.getOctokit(core.getInput('githubToken'));
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+
+  core.getInput('workflowEvent')
+  const workflowPayload = {
+    owner,
+    repo,
+    workflow_id: core.getInput('workflowID'),
+    status: 'success',
+    // branch: core.getInput('branch'),
+    event: core.getInput('workflowEvent'),
+  }
   octokit.actions
-    .listWorkflowRuns({
-      owner,
-      repo,
-      workflow_id: core.getInput("workflow_id"),
-      status: "success",
-      branch: core.getInput("branch"),
-      event: "push",
-    })
+    .listWorkflowRuns(workflowPayload)
     .then((res) => {
       const lastSuccessCommitHash =
         res.data.workflow_runs.length > 0
           ? res.data.workflow_runs[0].head_commit.id
-          : "";
-      core.setOutput("commit_hash", lastSuccessCommitHash);
+          : '';
+      core.setOutput('commitHash', lastSuccessCommitHash);
     })
     .catch((e) => {
       core.setFailed(e.message);
